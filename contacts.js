7f60b915207b2dff258438f4742fbccd16af781b
VladDyadenko <Stilmagroup@gmail.com>
2023-05-18 20:44:57 +0300

    added try-cath

--- contacts.js
+++ contacts.js
@@ -5,38 +5,58 @@ const { nanoid } = require("nanoid");
 const contactsPath = path.join(__dirname, "/db/contacts.json");
 
 const listContacts = async () => {
-  const data = await fs.readFile(contactsPath);
-  return JSON.parse(data);
+  try {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error("Помилка при отриманні списку контактів:", error);
+    return [];
+  }
 };
 
 const getContactById = async (contactId) => {
-  const data = await listContacts();
-  const res = data.find((vel) => vel.id === contactId);
-  return res || null;
+  try {
+    const data = await listContacts();
+    const res = data.find((vel) => vel.id === contactId);
+    return res || null;
+  } catch {
+    console.error("Помилка при отриманні контакту за ID:", error);
+    return null;
+  }
 };
 
 const removeContact = async (contactId) => {
-  const data = await listContacts();
-  const index = data.findIndex((vel) => vel.id === contactId);
-  if (index === -1) {
+  try {
+    const data = await listContacts();
+    const index = data.findIndex((vel) => vel.id === contactId);
+    if (index === -1) {
+      return null;
+    }
+    const [result] = data.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+    return result;
+  } catch {
+    console.error("Помилка при видаленні контакту:", error);
     return null;
   }
-  const [result] = data.splice(index, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
-  return result;
 };
 
 const addContact = async (name, email, phone) => {
-  const contacts = await listContacts();
-  const newContact = {
-    id: nanoid(),
-    name,
-    email,
-    phone,
-  };
-  contacts.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return newContact;
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch {
+    console.error("Помилка при додаванні контакту:", error);
+    return null;
+  }
 };
 
 module.exports = {
